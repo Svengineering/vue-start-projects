ce8f5d61af0f3febbef6ab87ff5f3f9137eaee9a
Svengineering <info@viaclara.io>
2025-10-16 12:03:28 +0200

    feat: next start project - a little weather app

---
+++ weather_app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="height:600px; width:800px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[44.48865, 11.3317]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+
+      <l-circle :lat-lng="[44.48865, 11.3317]" :radius="5000" color="green" />
+    </l-map>    
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import { LCircle, LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LCircle,
+  },
+  data() {
+    return {
+      zoom: 11,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
