740bbbf65a850f9eeb6aac876101691d25af2c19
Svengineering <info@viaclara.io>
2025-10-20 12:16:42 +0200

    trying to find out some Leaflet map event handling

--- weather_app/src/App.vue
+++ weather_app/src/App.vue
@@ -1,32 +1,54 @@
 <template>
   <div style="height:600px; width:800px">
-    <l-map ref="map" v-model:zoom="zoom" :center="[44.48865, 11.3317]">
+    <l-map 
+      ref="map" 
+      v-model:zoom="zoom" 
+      :center="[50.84, 4.39]"
+      @click="addMarker"
+      @update:center="updateCenter"
+      >
+
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
 
-      <l-circle :lat-lng="[44.48865, 11.3317]" :radius="5000" color="green" />
-    </l-map>    
+      <l-marker 
+        :lat-lng="markerPosition"
+        @click="console.log('Marker clicked')" />
+    </l-map>
   </div>
+  <div>Zoom ist {{ zoom }}</div>
+  <div>Marker Position ist {{ markerPosition }}</div>
 </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
-import L from 'leaflet';
-import { LCircle, LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import L, { marker } from 'leaflet';
+import { LCircle, LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
 
 export default {
   components: {
     LMap,
     LTileLayer,
     LCircle,
+    LMarker,
   },
   data() {
     return {
       zoom: 11,
+      markerPosition: [50.84, 4.39],
     };
+  },
+  methods: {
+    updateCenter(newCenter) {
+      //console.log("Map center updated to:", newCenter);
+      this.markerPosition = newCenter;
+    },
+    addMarker() {
+      console.log("Map clicked");
+    }
   },
 };
 </script>
