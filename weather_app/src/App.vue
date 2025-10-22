36eb8de75898f51746aafebd8f117c90bc315f44
Svengineering <info@viaclara.io>
2025-10-22 15:54:23 +0200

    feat: weather app - layout & design

--- weather_app/src/App.vue
+++ weather_app/src/App.vue
@@ -1,22 +1,27 @@
 <template>
-  <div style="height:600px; width:800px">
-    <l-map 
-      ref="map" 
-      v-model:zoom="zoom" 
-      :center="initialMapPosition"
-      >
+  <div class="hero">
+    <h1>Weather Service</h1>
+  </div>
+  <div class="map-section">
+    <div class="map-wrap">
+      <l-map 
+        ref="map" 
+        v-model:zoom="zoom" 
+        :center="initialMapPosition"
+        >
 
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      ></l-tile-layer>
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
 
-      <l-marker 
-        v-model:lat-lng="markerPosition"
-        v-bind:draggable="true"
-        @click="console.log('Marker clicked')" />
-    </l-map>
+        <l-marker 
+          v-model:lat-lng="markerPosition"
+          v-bind:draggable="true"
+          @click="console.log('Marker clicked')" />
+      </l-map>
+    </div>
   </div>
   <div>Zoom ist {{ zoom }}</div>
   <div>Marker Position ist {{ markerPosition }}</div>
@@ -77,4 +82,53 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+
+* {
+    box-sizing:border-box;
+}
+
+.hero {
+  height:66vh;
+  background-image:url('./assets/volcano-6.jpg');
+  background-size:cover;
+  background-position:0% 40%;
+  background-repeat: no-repeat;
+}
+
+h1 {
+  color:white;
+  font-family:sans-serif;
+  font-size:4rem;
+  padding-top:5%;
+  margin:0 auto;
+  max-width:1140px;
+}
+
+body {
+  margin:0;
+  padding:0;
+  /*background-color:#40968B;*/
+  /*background-color:#3c463a; */
+  background-color:#001a3e;
+
+}
+
+.map-wrap {
+  position:relative;
+  top:-250px;
+  border:10px solid white;
+  border-radius:20px;
+  box-shadow:0 0 20px rgba(0, 0, 0, 0.274);
+  max-width:60vw;
+  width:500px;
+  height:500px;
+}
+
+.map-section {
+  max-width:1140px;
+  margin:0 auto;
+}
+
+
+</style>
